076f7bfee4b792b2bad260477985e4fa2f97e25c
Kyle <kpat225@yahoo.com>
2020-05-31 18:33:33 -0400

    jp1.1

--- .bash_history
+++ .bash_history
@@ -3,3 +3,51 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install python3-pip
 sudo pip3 install adafruit-blinka
+git --verison
+git --version
+python3
+git init
+git remote add origin https://github.com/kylepconnolly/pi.git
+git add .
+git commit -m "jp 1.0"
+git config --global user.email "kpat225@yahoo.com"
+git config --global user.name "Kyle"
+git commit -m "jp 1.0"
+pwd
+git push origin master
+sudo raspi-config
+amixer cset numid=3 1
+sudo raspi-config
+amixer cset numid=3 1
+python3 jpnhelp.py
+amixer
+omxplayer -o local 1.mp3
+omxplayer -o local example.mp3
+amixer cset numid=3 1
+amixer cset numid=3 2
+amixer cset numid=3 0
+amixer cset numid=2 1
+omx
+omxplayer
+omxplayer -o --adev local
+omxplayer -o local
+omxplayer -
+omxplayer -o local
+omxplayer
+omxplayer -o hdmi
+omxplayer -o both
+omxplayer -p -o local test.mkv
+omxplayer -p -o local 1.mp3
+omxplayer -p -o local
+raspiconfig
+sudo raspi-config
+omxplayer -o local
+sudo apt-get -y install omxplayer
+omxplayer 1.mp3
+amixer cset numid=3 1
+apt-get install alsa-utils
+su
+sudo -i
+apt-get install mpg123
+apt-get install mpg123 -y
+sudo -i
--- jpnhelp.py
+++ jpnhelp.py
@@ -4,9 +4,17 @@ import board
 import digitalio
 
 def rndmp3 ():
-    randomfile = random.choice(os.listdir("/home/pi/music/"))
-    file = '/home/pi/music/'+ randomfile
-    os.system ('omxplayer' + file)                         
+    randomfile = random.choice(os.listdir("/home/pi/Music/"))
+    file = ' /home/pi/Music/'+ randomfile
+    os.system ('mpg123' + file)
+    
+def rndmp3 (mp3):
+    #mp3=rand
+    #rest of mp3 code
+    
+sig = None
+    
+print("file")
 
 button1 = digitalio.DigitalInOut(board.D5)
 button1.direction = digitalio.Direction.INPUT
@@ -16,13 +24,17 @@ button2 = digitalio.DigitalInOut(board.D6)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 
+
 while True:
-                               if not button1.value:
-                                   os.system('omxplayer rndmp3 &')
+    
+    if not button1.value:
+        rndmp3(sig)
+        print("button pressed")
                                
-                               if not button2.value:
-                                   os.system('omxplayer rndmp3.play &')
+    if not button2.value:
                                
-                               time.sleep(.25)
-            
-print("button pressed")
+    time.sleep(.25)
+                               
+
+
+
